d6fcd0596077a4ebd2c7949fee849d445d5dc7c1
guiyomj <wastfg6972@naver.com>
2021-11-02 16:53:29 +0900

    feat: custom radio button atom 구현

---
+++ frontend/src/components/atoms/Radio/index.tsx
@@ -0,0 +1,58 @@
+import styled from '@emotion/styled';
+
+import theme from '../../../styles/theme';
+
+const RadioBtnGroup = styled.label`
+  display: inline-flex;
+  align-items: center;
+  margin: 0.8rem;
+
+  > * {
+    cursor: pointer;
+    user-select: none;
+  }
+
+  > span {
+    margin-left: 0.2rem;
+  }
+
+  > input[type='radio'] {
+    --focus: 2px rgba(39, 94, 254, 0.3);
+    --border: #bbc1e1;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    width: 36px;
+    height: 36px;
+    outline: none;
+    display: inline-block;
+    position: relative;
+    margin: 0;
+    border-radius: 50%;
+    border: 1px solid ${theme.colors.gray};
+
+    &:hover {
+      border-color: ${theme.colors.deepgray};
+    }
+
+    &::after {
+      content: '';
+      position: absolute;
+      width: 33px;
+      height: 33px;
+      border-radius: 50%;
+      background: #fff;
+      transform: scale(0.5);
+    }
+  }
+`;
+
+const Radio = ({ name, value, checked }: any) => {
+  return (
+    <RadioBtnGroup>
+      <input type="radio" name={name} checked={checked} />
+      <span>{value}</span>
+    </RadioBtnGroup>
+  );
+};
+
+export default Radio;
