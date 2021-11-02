d53f89bc4bd2092c6ee2a440fba89e982ca68b76
guiyomj <wastfg6972@naver.com>
2021-11-02 23:33:59 +0900

    feat: 부덕이 프로필 색상 고르기 atom 추가
    재명님이 만들어 주신 charactor를 불러와서 부덕이 색 선택이 가능한 컴포넌트 생성
    16진수 랜덤값을 만들어서 rgb로 반환

---
+++ frontend/src/components/atoms/ProfileSelector/index.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+
+import styled from '@emotion/styled';
+
+import theme from '../../../styles/theme';
+import Character from '../character';
+
+const getRandomHex = () => {
+  return parseInt(Math.random() * 255)
+    .toString(16)
+    .padStart(2, '0');
+};
+
+const ProfileContainer = styled.div`
+  border-radius: 50%;
+  border: 1px solid ${theme.colors.gray};
+  max-width: 240px;
+  max-height: 240px;
+  width: 24vw;
+  height: 24vw;
+  margin: 0 auto 2.4rem auto;
+  position: relative;
+`;
+
+const ChangeBtn = styled.span`
+  border-radius: 50%;
+  max-width: 80px;
+  max-height: 80px;
+  width: 8vw;
+  height: 8vw;
+  cursor: pointer;
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  border: 1px solid ${theme.colors.deepgray};
+  background: #fff url('images/ic_shuffle.png') no-repeat center/45%;
+`;
+
+const ProfileSelector = () => {
+  const [color, setColor] = useState('C9DAF8');
+
+  const getRandomColor = () => {
+    setColor(`${getRandomHex()}${getRandomHex()}${getRandomHex()}`);
+  };
+
+  return (
+    <ProfileContainer>
+      <Character width={'100%'} height={'100%'} color={color}></Character>
+      <ChangeBtn onClick={getRandomColor} />
+    </ProfileContainer>
+  );
+};
+
+export default ProfileSelector;
