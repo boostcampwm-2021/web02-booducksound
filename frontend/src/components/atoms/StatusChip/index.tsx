6d2e059b3be7a041b625c46b60ee6038828bd995
leetaewook <zsc4578@naver.com>
2021-11-02 17:47:15 +0900

    feat: StatusChip atom 작성

---
+++ frontend/src/components/atoms/StatusChip/index.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+import theme from '../../../styles/theme';
+
+interface Props {
+  status: 'king' | 'ready' | 'prepare';
+}
+
+const chipColor = {
+  king: theme.colors.orange,
+  ready: theme.colors.ocean,
+  prepare: theme.colors.lilac,
+};
+
+const statusKorean = {
+  king: '방장',
+  ready: '준비완료',
+  prepare: '준비중',
+};
+
+const Chip = styled.div<Props>`
+  display: inline-block;
+  border: 1px solid ${({ status }) => chipColor[status]};
+  border-radius: 4px;
+  font-size: 12px;
+  color: ${({ status }) => chipColor[status]};
+  padding: 2px 6px;
+`;
+
+const StatusChip = ({ status }: Props) => {
+  return <Chip status={status}>{statusKorean[status]}</Chip>;
+};
+
+export default StatusChip;
