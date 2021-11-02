bf19252b9381c354eb23b4937daddfc43fa59c12
leetaewook <zsc4578@naver.com>
2021-11-02 18:52:45 +0900

    fix: 컨벤션에 맞지 않는 코드들 수정

--- frontend/src/components/atoms/GlassContainer/index.tsx
+++ frontend/src/components/atoms/GlassContainer/index.tsx
@@ -1,7 +1,9 @@
+import { PropsWithChildren } from 'react';
+
 import styled from '@emotion/styled';
+
 import theme from '../../../styles/theme';
 
-import { PropsWithChildren } from 'react';
 const Container = styled.div`
   width: 100%;
   padding: 1%;
