860293b17b0edb23afee67f6e9a3c80bf51f29c8
vgihan <jum654@naver.com>
2021-11-03 22:09:02 +0900

    feat: DeleteButton atom 컴포넌트 개발
    [#1]

---
+++ frontend/src/components/atoms/DeleteButton/index.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+const DeleteButton = styled.button`
+  background-image: url('/images/ic_delete.svg');
+  width: 16px;
+  height: 16px;
+  font-size: inherit;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
+export default DeleteButton;
