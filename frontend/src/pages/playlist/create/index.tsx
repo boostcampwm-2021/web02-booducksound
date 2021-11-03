05877c59ba60778ebafb39baf8f998120f5788a4
vgihan <jum654@naver.com>
2021-11-04 00:08:47 +0900

    feat: 플레이리스트 추가 페이지 구현
    - CreatePlaylistInputBox organisms, CreatePlaylistMusicList organisms
      컴포넌트를 조합하여 구현.
    - 브라우저 크기에 따라 PageBox를 반응형으로 조절 가능하도록 구현

--- frontend/src/pages/playlist/create/index.tsx
+++ frontend/src/pages/playlist/create/index.tsx
@@ -4,37 +4,14 @@ import styled from '@emotion/styled';
 import type { NextPage } from 'next';
 
 import Button from '../../../components/atoms/Button';
-import Chip from '../../../components/atoms/Chip';
 import MenuInfoBox from '../../../components/atoms/MenuInfoBox';
 import PageBox from '../../../components/atoms/PageBox';
-import InputSection from '../../../components/molecules/InputSection';
+import Chip from '../../../components/molecules/Chip';
+import CreatePlaylistInputBox from '../../../components/organisms/CreatePlaylistInputBox';
+import CreatePlaylistMusicList from '../../../components/organisms/CreatePlaylistMusicList';
 import theme from '../../../styles/theme';
+import { Music } from '../../../types/music';
 
-// 반응형으로 처리할 것 : InputSection
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 20px;
-  @media (max-width: 1200px) {
-    & > div > :last-child {
-      height: 40px;
-      padding-left: 30px;
-    }
-  }
-  @media (max-width: 768px) {
-    & > div > :last-child {
-      height: 40px;
-      padding-left: 20px;
-    }
-  }
-  @media (max-width: 480px) {
-    & > div > :last-child {
-      height: 35px;
-      padding-left: 10px;
-    }
-  }
-`;
 const ChipContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -47,46 +24,9 @@ const Wrapper = styled.div`
   flex-direction: column;
   row-gap: 20px;
 `;
-const MusicListContainer = styled.div``;
-const MusicListTitleBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 15px;
-  border-bottom: 2px solid #eee;
-`;
-const MusicListContentBox = styled.div`
-  height: 350px;
-  border-bottom: 2px solid #eee;
-`;
-const MusicListTitleTop = styled.div`
-  display: flex;
-  align-items: center;
-  column-gap: 5px;
-  @media (max-width: 1200px) {
-    column-gap: 30px;
-  }
-  @media (max-width: 768px) {
-    column-gap: 20px;
-  }
-  @media (max-width: 480px) {
-    column-gap: 10px;
-  }
-`;
-const MusicListTitleBottom = styled.div`
-  color: ${theme.colors.gray};
-  margin-bottom: 30px;
-`;
-const MusicListTitle = styled.div`
-  font-weight: bold;
-  @media (min-width: 480px) {
-    font-size: 15px;
-  }
-  @media (min-width: 768px) {
-    font-size: 20px;
-  }
-  @media (min-width: 1200px) {
-    font-size: 25px;
-  }
+const SubmitButtonWrapper = styled.div`
+  display: inline;
+  text-align: center;
 `;
 const Container = styled.div`
   @media (max-width: 1200px) {
@@ -110,71 +50,29 @@ const Container = styled.div`
 `;
 
 const PlaylistCreate: NextPage = () => {
-  const [chips, setChips] = useState(['asdf', 'wviasd', 'adf']);
+  const [chips, setChips] = useState<string[]>([]);
+  const [musics, setMusics] = useState<Music[]>([]);
   return (
     <Container>
       <MenuInfoBox name={'플레이리스트 추가'}></MenuInfoBox>
       <PageBox>
         <Wrapper>
-          <InputContainer>
-            <InputSection
-              id={'playlist-title'}
-              title={'플레이리스트 제목'}
-              titleSize={'1em'}
-              isSearch={false}
-              placeholder={'플레이리스트 제목을 입력해주세요'}
-              width={'100%'}
-              height={'3em'}
-              fontSize={'0.85em'}
-              margin={'0.6em'}
-              paddingW={'40px'}
-            ></InputSection>
-            <InputSection
-              id={'playlist-title'}
-              title={'플레이리스트 설명'}
-              titleSize={'1em'}
-              isSearch={false}
-              placeholder={'플레이리스트 설명을 입력해주세요'}
-              width={'100%'}
-              height={'3em'}
-              fontSize={'0.85em'}
-              margin={'0.6em'}
-              paddingW={'40px'}
-            ></InputSection>
-            <InputSection
-              id={'playlist-title'}
-              title={'플레이리스트 해시태그'}
-              titleSize={'1em'}
-              isSearch={false}
-              placeholder={'추가할 해시태그를 입력 후 Enter를 클릭하세요'}
-              width={'100%'}
-              height={'3em'}
-              fontSize={'0.85em'}
-              margin={'0.6em'}
-              paddingW={'40px'}
-            ></InputSection>
-          </InputContainer>
+          <CreatePlaylistInputBox />
           <ChipContainer>
             {chips.map((chip, idx) => (
-              <Chip color={theme.colors.soda} content={chip} fontSize={'0.8em'} key={idx}></Chip>
+              <Chip content={chip} key={idx}></Chip>
             ))}
           </ChipContainer>
-          <MusicListContainer>
-            <MusicListTitleBox>
-              <MusicListTitleTop>
-                <MusicListTitle>노래 목록</MusicListTitle>
-                <Button
-                  content={'추가'}
-                  background={theme.colors.mint}
-                  fontSize={'12px'}
-                  paddingH={'7px'}
-                  width={'100px'}
-                ></Button>
-              </MusicListTitleTop>
-              <MusicListTitleBottom>최소 3개, 최대 50개까지 추가가 가능합니다.</MusicListTitleBottom>
-            </MusicListTitleBox>
-            <MusicListContentBox></MusicListContentBox>
-          </MusicListContainer>
+          <CreatePlaylistMusicList musics={musics}></CreatePlaylistMusicList>
+          <SubmitButtonWrapper>
+            <Button
+              content={'등록'}
+              background={theme.colors.sky}
+              fontSize={'1.5em'}
+              paddingH={'2%'}
+              width={'45%'}
+            ></Button>
+          </SubmitButtonWrapper>
         </Wrapper>
       </PageBox>
     </Container>
