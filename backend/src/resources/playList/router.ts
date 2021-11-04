d5199a6350e53ff0a6298f9bbd746fa71f97bc3f
coreight98 <maxcha98@naver.com>
2021-11-04 18:31:08 +0900

    fix : restapi modify url

--- backend/src/resources/playList/router.ts
+++ backend/src/resources/playList/router.ts
@@ -5,8 +5,8 @@ import playList from './controller';
 const router = express.Router();
 
 // router.get('playList/filter', playList.filter);
-router.post('/register', playList.register);
-router.put('/modify', playList.modify);
-router.delete('/delete', playList.del);
+router.post('/', playList.register);
+router.put('/', playList.modify);
+router.delete('/', playList.del);
 
 export default router;
