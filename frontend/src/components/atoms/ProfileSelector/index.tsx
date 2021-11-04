83410e36ba92a55bc1da441b8b6b1e603c83fd47
guiyomj <wastfg6972@naver.com>
2021-11-04 23:33:09 +0900

    refactor: profileSelector props 변경
    부모 컴포넌트에서 color, setColor 받아오도록 변경

--- frontend/src/components/atoms/ProfileSelector/index.tsx
+++ frontend/src/components/atoms/ProfileSelector/index.tsx
@@ -1,4 +1,4 @@
-import { useState, PropsWithChildren } from 'react';
+import { useState, PropsWithChildren, MouseEventHandler } from 'react';
 
 import styled from '@emotion/styled';
 
@@ -7,6 +7,8 @@ import Character from '../Character';
 
 interface Props {
   type?: string;
+  color: string;
+  setColor: any;
 }
 
 const getRandomHex = () => {
@@ -40,9 +42,7 @@ const ChangeBtn = styled.span<Props>`
   background: #fff url('images/ic_shuffle.png') no-repeat center/45%;
 `;
 
-const ProfileSelector = ({ type }: PropsWithChildren<Props>) => {
-  const [color, setColor] = useState('C9DAF8');
-
+const ProfileSelector = ({ type, color, setColor }: PropsWithChildren<Props>) => {
   const getRandomColor = () => {
     setColor(`${getRandomHex()}${getRandomHex()}${getRandomHex()}`);
   };
