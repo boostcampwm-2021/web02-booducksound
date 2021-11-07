22e39739a8c46b178d1490c38dce289a27759224
vgihan <jum654@naver.com>
2021-11-07 16:51:14 +0900

    refactor: InputBox 컴포넌트 수정본 InputText 구현
    - Props로 스타일 속성을 전달 받았던 기존 형식에서, styled(component)
      형식으로 확장해서 사용할 수 있는 컴포넌트로 수정함.
    - div 태그 아래에 input 태그를 사용하여 만들었던 기존 방식에서, Input
      태그 하나만으로 구현

---
+++ frontend/src/components/atoms/InputText/index.tsx
@@ -0,0 +1,40 @@
+import { ChangeEventHandler, KeyboardEventHandler } from 'react';
+
+import styled from '@emotion/styled';
+
+interface Props {
+  isSearch: boolean;
+  placeholder: string;
+  className: string;
+  value: string;
+  enterHandler: KeyboardEventHandler;
+  changeHandler: ChangeEventHandler;
+}
+interface ContainerProps {
+  isSearch: boolean;
+}
+
+const Container = styled.input<ContainerProps>`
+  border: 2px solid black;
+  border-radius: 100px;
+  outline: none;
+  background-image: ${({ isSearch }) => (isSearch ? "url('/images/readingGlass.png')" : '')};
+  background-repeat: no-repeat;
+  background-position: left;
+`;
+
+const InputText = ({ isSearch, placeholder, className, value, enterHandler, changeHandler }: Props) => {
+  return (
+    <Container
+      className={className}
+      isSearch={isSearch}
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onKeyUp={enterHandler}
+      onChange={changeHandler}
+    ></Container>
+  );
+};
+
+export default InputText;
