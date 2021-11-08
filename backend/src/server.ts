1a55e1983f97d4094d8b3bb4c77fac18b0fe2415
leetaewook <zsc4578@naver.com>
2021-11-08 22:36:57 +0900

    fix: typo correction

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,7 +7,7 @@ import morgan from 'morgan';
 
 import io from './io';
 import accRouter from './resources/account/router';
-import playListRouter from './resources/playList/router';
+import playlistRouter from './resources/playList/router';
 
 const app = express();
 
