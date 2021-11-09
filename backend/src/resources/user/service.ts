0f74b9ac8db0501f24a72044e5aaf2bf793a0ce7
vgihan <jum654@naver.com>
2021-11-09 17:30:20 +0900

    feat: 플레이리스트 생성 시 User myPlaylist에 추가
    - /playlist POST 요청 시 UserService를 이용하여 myPlaylist를 update
      시킴.
    
    [#7]

--- backend/src/resources/user/service.ts
+++ backend/src/resources/user/service.ts
@@ -1,4 +1,6 @@
-import Playlist from '../../models/Playlist';
+import mongoose from 'mongoose';
+
+import Playlist from '../../models/PlayList';
 import User from '../../models/User';
 
 const changeColor = async (id: string, color: string) => {
@@ -11,6 +13,15 @@ const getMyPlaylist = async (_id: string) => {
   return play;
 };
 
+const postMyPlaylist = async (userId: string, playlistId: mongoose.Types.ObjectId) => {
+  await User.findOneAndUpdate(
+    {
+      id: userId,
+    },
+    { $push: { myPlaylist: playlistId } },
+  );
+};
+
 const deleteLikes = async (id: string, _id: string) => {
   await User.updateOne({ id }, { $pull: { likes: _id } });
 };
@@ -19,4 +30,5 @@ export default {
   changeColor,
   getMyPlaylist,
   deleteLikes,
+  postMyPlaylist,
 };
