10ca8c9b5e917bf4cf92a088a4554fd0cd77469f
vgihan <jum654@naver.com>
2021-11-09 21:28:20 +0900

    refactor: 렌더링 최적화를 위한 state 결합
    - 기존 플레이리스트를 수정하기 위해 input value에 각각 상태 값을
      넣어주어야 함.
    - 상태 값이 나누어져있으면, 해당 수 만큼 렌더링 되기 때문에 렌더링 수를
      줄이기 위해 상태를 하나로 모아주는 리팩토링을 진행함.
    
    [#10]

--- frontend/src/pages/playlist/create/index.tsx
+++ frontend/src/pages/playlist/create/index.tsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { KeyboardEventHandler, MouseEvent, useState } from 'react';
 
 import styled from '@emotion/styled';
 import type { NextPage } from 'next';
-import Router from 'next/router';
+import Router, { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
 
 import { createPlaylist } from '~/api/playlist';
@@ -10,13 +10,13 @@ import Button from '~/atoms/Button';
 import MenuInfoBox from '~/atoms/MenuInfoBox';
 import PageBox from '~/atoms/PageBox';
 import { FAILED, SUCCESS } from '~/constants/index';
-import useEventListener from '~/hooks/useEventListener';
 import Chip from '~/molecules/Chip';
 import CreatePlaylistInputBox from '~/organisms/CreatePlaylistInputBox';
 import CreatePlaylistMusicList from '~/organisms/CreatePlaylistMusicList';
 import CreatePlaylistMusicModal from '~/organisms/CreatePlaylistMusicModal';
 import theme from '~/styles/theme';
 import { Music } from '~/types/Music';
+import { PlaylistInput } from '~/types/PlaylistInput';
 
 const ChipContainer = styled.div`
   display: flex;
@@ -62,36 +62,40 @@ interface ModalOption {
 }
 
 const PlaylistCreate: NextPage = () => {
-  const [title, setTitle] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const [hashTag, setHashTag] = useState<string>('');
-  const [chips, setChips] = useState<string[]>([]);
-  const [musics, setMusics] = useState<Music[]>([]);
-  const [modalOption, setModalOption] = useState<ModalOption>({ type: 'close', target: TARGET_INIT });
   const userInfo = useSelector((state: any) => state.user);
+  const [modalOption, setModalOption] = useState<ModalOption>({ type: 'close', target: TARGET_INIT });
+  const [playlist, setPlaylistAll] = useState<PlaylistInput>({
+    playlistName: '',
+    description: '',
+    hashTag: '',
+    hashTags: [],
+    musics: [],
+  });
+  const { playlistName, description, hashTag, hashTags, musics } = playlist;
 
+  const setPlaylist = (newState: Object) =>
+    setPlaylistAll((preState) => {
+      return { ...preState, ...newState };
+    });
   const checkAllValidInput = () => {
-    if (!title || !description || !chips.length) return false;
+    if (!playlistName || !description || !hashTags.length) return false;
     if (musics.length < 3 || musics.length > 50) return false;
     return true;
   };
-  const handleAddChip = (e: globalThis.KeyboardEvent) => {
+  const handleAddChip: KeyboardEventHandler = (e) => {
     if (e.key !== 'Enter' && e.key !== ' ') return;
     if (!hashTag) return;
-    setHashTag('');
-    setChips((preState) => [...preState, hashTag.trim()]);
+    setPlaylist({ hashTag: '' });
+    setPlaylistAll((preState: PlaylistInput) => {
+      return { ...preState, hashTags: [...preState.hashTags, hashTag.trim()] };
+    });
   };
   const handleSubmit = async () => {
     if (!checkAllValidInput()) {
       alert('입력을 확인해주세요.');
       return;
     }
-    const createResult = await createPlaylist({
-      playlistName: title,
-      musics: musics,
-      hashTags: chips,
-      userId: userInfo.id,
-    });
+    const createResult = await createPlaylist({ playlistName, musics, hashTags, userId: userInfo.id });
     const { status } = createResult;
     if (status === FAILED) {
       alert('플레이리스트 등록에 실패하였습니다.');
@@ -99,7 +103,24 @@ const PlaylistCreate: NextPage = () => {
       Router.push('/lobby');
     }
   };
-  useEventListener('keyup', handleAddChip);
+  const addMusics = (newMusic: Music) => {
+    setPlaylistAll((preState: PlaylistInput) => {
+      return { ...preState, musics: [...preState.musics, newMusic] };
+    });
+  };
+  const updateMusics = (newMusic: Music) => {
+    setPlaylistAll((preState: PlaylistInput) => {
+      const nextMusics = [...preState.musics];
+      nextMusics[modalOption.target] = newMusic;
+      return { ...preState, musics: nextMusics };
+    });
+  };
+  const deleteMusics = (idx: number) => (e: MouseEvent) => {
+    setPlaylistAll((preState: PlaylistInput) => {
+      const nextMusics = preState.hashTags.filter((hashTag, i) => i !== idx);
+      return { ...preState, hashTags: [...nextMusics] };
+    });
+  };
 
   return (
     <Container>
@@ -107,29 +128,23 @@ const PlaylistCreate: NextPage = () => {
       <PageBox>
         <Wrapper>
           <CreatePlaylistInputBox
-            setTitle={(e) => setTitle((e.currentTarget as HTMLTextAreaElement).value)}
-            setDescription={(e) => setDescription((e.currentTarget as HTMLTextAreaElement).value)}
-            setHashTag={(e) => setHashTag((e.currentTarget as HTMLTextAreaElement).value)}
-            title={title}
+            setPlaylist={setPlaylist}
+            handleAddChip={handleAddChip}
+            playlistName={playlistName}
             description={description}
             hashTag={hashTag}
           />
           <ChipContainer>
-            {chips.map((chip, idx) => (
-              <Chip
-                key={idx}
-                deleteHandler={(e) => {
-                  setChips((preState) => [...preState.filter((chip, i) => i !== idx)]);
-                }}
-              >
-                {chip}
+            {hashTags.map((hashTag, idx) => (
+              <Chip key={idx} deleteHandler={deleteMusics(idx)}>
+                {hashTag}
               </Chip>
             ))}
           </ChipContainer>
           <CreatePlaylistMusicList
             musics={musics}
             setModalOption={setModalOption}
-            setMusics={setMusics}
+            setPlaylist={setPlaylist}
           ></CreatePlaylistMusicList>
           <SubmitButtonWrapper>
             <Button
@@ -144,20 +159,11 @@ const PlaylistCreate: NextPage = () => {
         </Wrapper>
       </PageBox>
       {modalOption.type === 'create' ? (
-        <CreatePlaylistMusicModal
-          setModalOption={setModalOption}
-          setMusics={(newMusic: Music) => setMusics((preMusics) => [...preMusics, newMusic])}
-        ></CreatePlaylistMusicModal>
+        <CreatePlaylistMusicModal setModalOption={setModalOption} setMusics={addMusics}></CreatePlaylistMusicModal>
       ) : modalOption.type === 'modify' ? (
         <CreatePlaylistMusicModal
           setModalOption={setModalOption}
-          setMusics={(newMusic: Music) =>
-            setMusics((preMusics) => {
-              const next = [...preMusics];
-              next[modalOption.target] = newMusic;
-              return next;
-            })
-          }
+          setMusics={updateMusics}
           musicInfo={musics[modalOption.target]}
         ></CreatePlaylistMusicModal>
       ) : null}
