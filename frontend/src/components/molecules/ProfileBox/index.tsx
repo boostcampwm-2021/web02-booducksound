06525b83b269f9f96c4234b1662cdf1b6c8aee2e
coreight98 <maxcha98@naver.com>
2021-11-11 05:40:54 +0900

    feat : add molecules to improve myPage

---
+++ frontend/src/components/molecules/ProfileBox/index.tsx
@@ -0,0 +1,91 @@
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+import { requestLogout } from '~/api/account';
+import Button from '~/atoms/Button';
+import UserInfoBox from '~/molecules/UserInfoBox';
+import theme from '~/styles/theme';
+
+const Warper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-bottom: 1px solid ${theme.colors.gray};
+
+  > div:first-of-type {
+    flex-grow: 0;
+  }
+
+  > div:last-child {
+    flex-grow: 1;
+  }
+
+  @media (max-width: 480px) {
+    display: block;
+  }
+`;
+const ProfileBtnBox = styled.div`
+  text-align: right;
+
+  @media (min-width: 768px) {
+    margin-bottom: 1rem;
+
+    > *:first-of-type {
+      margin-right: 1rem;
+    }
+  }
+
+  @media (max-width: 768px) {
+    button {
+      margin-bottom: 0.4rem;
+    }
+  }
+  @media (max-width: 480px) {
+    margin-bottom: 1rem;
+
+    button {
+      padding: 8px 0;
+      font-size: 0.8rem;
+      width: 90%;
+      display: block;
+      margin: 0 auto 0.4rem auto;
+    }
+  }
+`;
+type Info = {
+  id: string;
+  nickname: string;
+  userColor: string;
+};
+
+const ProfileBox = ({ info, handleUserMenu }: { info: Info; handleUserMenu: Function }) => {
+  const { id, nickname, userColor } = info;
+  return (
+    <Warper>
+      <UserInfoBox info={{ id, nickname, userColor }} />
+      <ProfileBtnBox>
+        {handleUserMenu(
+          id,
+          <Link href="/findPwd">
+            <a>
+              <Button background={theme.colors.lime} fontSize={'16px'} paddingH={'16px'} width={'160px'}>
+                비밀번호 변경
+              </Button>
+            </a>
+          </Link>,
+        )}
+        <Button
+          background={theme.colors.lightsky}
+          fontSize={'16px'}
+          paddingH={'16px'}
+          width={'160px'}
+          onClick={requestLogout}
+        >
+          로그아웃
+        </Button>
+      </ProfileBtnBox>
+    </Warper>
+  );
+};
+
+export default ProfileBox;
