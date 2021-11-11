c9f683a348020962361ef58bfa448c0e43194377
guiyomj <wastfg6972@naver.com>
2021-11-11 16:52:16 +0900

    feat: 로그아웃시 store 정보 삭제 구현

--- frontend/src/reducers/user.ts
+++ frontend/src/reducers/user.ts
@@ -25,6 +25,9 @@ const userReducer = (state = initialState, action: { type: UserActions; payload:
       const { id, nickname, color, likes, myPlaylist } = payload;
       return { ...state, id, nickname, color, likes, myPlaylist };
     }
+    case UserActions.REMOVE_USER: {
+      return initialState;
+    }
     case UserActions.SET_COLOR: {
       const { color } = payload;
       return { ...state, color };
