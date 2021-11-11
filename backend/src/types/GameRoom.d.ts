1f4d6055539b50c07b68d1178dfc810c1fcaa528
coreight98 <maxcha98@naver.com>
2021-11-11 21:56:19 +0900

    feat : add new type for change option

--- backend/src/types/GameRoom.d.ts
+++ backend/src/types/GameRoom.d.ts
@@ -3,8 +3,9 @@ import { Player } from './Player';
 export type GameRoom = {
   status: 'playing' | 'waiting';
   players: { [socketId: string]: Player };
+  playlistName: string;
   title: string;
-  playListId: string;
+  playlistId: string;
   hasPassword: boolean;
   skip: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
   timePerProblem: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90;
--- backend/src/types/ServerRoom.d.ts
+++ backend/src/types/ServerRoom.d.ts
@@ -4,7 +4,8 @@ export type ServerRoom = {
   status: 'playing' | 'waiting';
   players: { [socketId: string]: Player };
   title: string;
-  playListId: string;
+  playlistName: string;
+  playlistId: string;
   password?: string | null;
   skip: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
   timePerProblem: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90;
