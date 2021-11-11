423ae2291eee3d491706ddd80e0ab3ce2663b85b
guiyomj <wastfg6972@naver.com>
2021-11-11 13:46:27 +0900

    refactor: 사용자 정보 불러오기 리팩토링
    기존 야매 FC에서 useDisfetch를 사용하여 액션 호출하던 방식을 MyApp 최상위함수에서 하도록 수정

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -3,34 +3,27 @@ import { useEffect } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app';
 import { Provider, useDispatch } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose, Action } from 'redux';
+import thunk, { ThunkDispatch } from 'redux-thunk';
 
 import { getUser } from '~/actions/user';
 import { handleLoginUser } from '~/api/account';
 import rootReducer from '~/reducers/index';
+import { UserState } from '~/reducers/user';
 import theme from '~/styles/theme';
 
 import '~/styles/default.css';
 
 const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
-const Authenticator = () => {
-  const dispatch = useDispatch();
-
+const MyApp = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
-    dispatch(getUser());
+    (store.dispatch as ThunkDispatch<UserState, void, Action>)(getUser());
     handleLoginUser();
   }, []);
-
-  return <></>;
-};
-
-const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <Authenticator />
         <Component {...pageProps} />
       </Provider>
     </ThemeProvider>
