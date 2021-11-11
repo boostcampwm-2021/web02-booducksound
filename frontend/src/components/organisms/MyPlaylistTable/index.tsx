88383300c4b9049d092a96211bcf72428555e5f8
vgihan <jum654@naver.com>
2021-11-11 16:48:59 +0900

    feat: mypage에서 myPlaylist 삭제 기능 구현
    - drawMyPlaylistBox 함수를 통해 컴포넌트를 리턴해주는 기존 방식에서,
      MyPlaylistTable organisms 컴포넌트로 분리.
    - 삭제 성공 시, dispatch 함수를 통해 전역 상태를 업데이트 시켜주어
      페이지를 리렌더링 함.

---
+++ frontend/src/components/organisms/MyPlaylistTable/index.tsx
@@ -0,0 +1,100 @@
+import { Dispatch, MouseEventHandler } from 'react';
+
+import styled from '@emotion/styled';
+import router from 'next/router';
+import { useDispatch } from 'react-redux';
+
+import { getUser } from '~/actions/user';
+import { deletePlaylist } from '~/api/playlist';
+import Button from '~/atoms/Button';
+import { FAILED, PLAYLIST_EMPTY_MSG } from '~/constants/index';
+import theme from '~/styles/theme';
+import { Playlist } from '~/types/Playlist';
+
+const EmptyPlayList = styled.tr`
+  border: 1px solid ${theme.colors.gray};
+  border-width: 1px 0 1px 0;
+  color: ${theme.colors.gray};
+
+  > td {
+    padding: 8rem 0 !important;
+  }
+`;
+const PlayTitle = styled.h4`
+  text-align: left;
+  font-size: 1.2rem;
+`;
+const TableBtnBox = styled.div`
+  > button {
+    margin-bottom: 0.4rem;
+  }
+`;
+
+interface Props {
+  myPlaylist: Playlist[] | any;
+  isMine: boolean;
+  openRemoveModal: ({ target }: any) => void;
+}
+
+const MyPlaylistTable = ({ myPlaylist, isMine, openRemoveModal }: Props) => {
+  const dispatch = useDispatch();
+
+  const handleDeletePlaylist =
+    (_id: string | undefined, dispatch: Dispatch<any>): MouseEventHandler =>
+    async (e) => {
+      if (!_id) return;
+      const result = await deletePlaylist(_id);
+      if (result.status === FAILED) return alert('알 수 없는 오류로 삭제되지 않았습니다.');
+      dispatch(getUser());
+    };
+  const handleUpdatePlaylist =
+    (_id: string | undefined): MouseEventHandler =>
+    (e) => {
+      if (!_id) return;
+      router.push(`/playlist/${_id}`);
+    };
+
+  return (
+    <tbody>
+      {!myPlaylist.length ? (
+        <EmptyPlayList className="no-result">
+          <td colSpan={2}>{PLAYLIST_EMPTY_MSG}</td>
+        </EmptyPlayList>
+      ) : (
+        myPlaylist.map((e: Playlist) => (
+          <tr key={e._id} data-id={e._id} data-writer={e.userId}>
+            <td>
+              <PlayTitle>{e.playlistName}</PlayTitle>
+            </td>
+            <td>
+              <TableBtnBox>
+                {isMine && (
+                  <Button
+                    background={theme.colors.sky}
+                    fontSize={'14px'}
+                    paddingH={'8px'}
+                    width={'100px'}
+                    onClick={handleUpdatePlaylist(e._id)}
+                  >
+                    수정
+                  </Button>
+                )}
+                <Button
+                  background={theme.colors.peach}
+                  fontSize={'14px'}
+                  paddingH={'8px'}
+                  width={'100px'}
+                  onClick={isMine ? handleDeletePlaylist(e._id, dispatch) : openRemoveModal}
+                >
+                  삭제
+                </Button>
+              </TableBtnBox>
+            </td>
+          </tr>
+        ))
+      )}
+    </tbody>
+  );
+};
+
+export default MyPlaylistTable;
