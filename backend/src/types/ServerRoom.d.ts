85d40e4e63aefd0f036bf40753ab4b24d4897b9d
vgihan <jum654@naver.com>
2021-11-11 18:44:29 +0900

    fix: 네이밍 컨벤션에 맞게 변수 이름 수정
    - PlayList to Playlist

--- backend/src/types/ServerRoom.d.ts
+++ backend/src/types/ServerRoom.d.ts
@@ -6,12 +6,12 @@ export type ServerRoom = {
   status: 'playing' | 'waiting';
   players: { [socketId: string]: Player };
   title: string;
-  playListId: string;
+  playlistId: string;
   password?: string | null;
   skip: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
   timePerProblem: 10 | 20 | 30 | 40 | 50 | 60 | 70 | 80 | 90;
 
-  musics: { youtubeId: string; answers: string[]; hint: string }[];
+  musics: { url: string; info: string; answers: string[]; hint: string }[];
   streams: PassThrough[];
   curRound: number;
   maxRound: number;
