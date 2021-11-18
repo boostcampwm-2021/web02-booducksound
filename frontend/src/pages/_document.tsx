6ea2379b200d88e6cc192e1df2b9c6b4ffbb1677
guiyomj <wastfg6972@naver.com>
2021-11-18 14:55:31 +0900

    feat: 검색엔진 최적화를 위한 메타태그 추가
    네이버, 구글 사이트 확인 메타태그 추가

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -11,6 +11,8 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <meta name="keywords" content="온라인,음악게임,노래맞추기,부덕사운드,booducksound,네이버,부스트캠프" />
           <meta name="description" content="🐥온라인 노래 맞히기 게임 플랫폼 부덕사운드 입니다.🐥" />
+          <meta name="naver-site-verification" content="bf0aa56a1eb42c038834ca4aafb118b5d8bf15af" />
+          <meta name="google-site-verification" content="QL6xJnBIBwJARnRjGuBW1uDQS98EJZFiFUkfUAxJm54" />
           <link rel="short icon" href="images/favicon.ico" />
         </Head>
         <body>
