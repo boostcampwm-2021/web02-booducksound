e158a140694b70efecd57c55695c995373b917ac
vgihan <jum654@naver.com>
2021-11-23 20:52:55 +0900

    refactor: Youtubestream 리팩토링
    - constructor 로직을 함수로 분리함

--- backend/src/variables/YoutubeStream.ts
+++ backend/src/variables/YoutubeStream.ts
@@ -14,17 +14,16 @@ class Youtubestream {
   #passThrough?: PassThrough;
 
   constructor(url: string) {
-    const MAX_TIME_LENGTH = 90;
-    const regex = /(.*?)(^|\/|v=)([a-z0-9_-]{11})(.*)?/i;
-    const videoId = (url.match(regex) as RegExpExecArray)[3];
-    const mp3Path = `musics/${videoId}.mp3`;
+    this.videoId = this.parseUrl(url);
+    this.mp3Path = this.getMp3Path(this.videoId);
+    this.#passThrough = this.getPassThrough(this.videoId, this.mp3Path);
+  }
 
-    this.videoId = videoId;
-    this.mp3Path = mp3Path;
-
+  getPassThrough(videoId: string, mp3Path: string) {
     if (!fs.existsSync(`musics`)) fs.mkdirSync('musics');
     if (fs.existsSync(mp3Path) && fs.statSync(mp3Path).size > 0) return;
 
+    const MAX_TIME_LENGTH = 90;
     const passThrough = new PassThrough();
     const writeStream = fs.createWriteStream(mp3Path);
     const video = ytdl(`https://youtube.com/watch?v=${videoId}`, { quality: 'lowestaudio' });
@@ -39,7 +38,16 @@ class Youtubestream {
       passThrough.pipe(writeStream);
     });
 
-    this.#passThrough = passThrough;
+    return passThrough;
+  }
+
+  getMp3Path(videoId: string) {
+    return `musics/${videoId}.mp3`;
+  }
+
+  parseUrl(url: string) {
+    const regex = /(.*?)(^|\/|v=)([a-z0-9_-]{11})(.*)?/i;
+    return (url.match(regex) as RegExpExecArray)[3];
   }
 
   get stream() {
