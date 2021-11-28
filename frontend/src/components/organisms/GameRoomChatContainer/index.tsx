b8e21c76e065671f33356fa47f75cc5a6c2b07c2
vgihan <jum654@naver.com>
2021-11-28 16:30:21 +0900

    fix: chat DOM Element 갯수 조정 로직 추가
    - 채팅이 많이 쌓일 경우 50개까지만 보여주고, 이후 무한 스크롤을 통해
      이전 기록을 참조할 수 있도록 개선함.

--- frontend/src/components/organisms/GameRoomChatContainer/index.tsx
+++ frontend/src/components/organisms/GameRoomChatContainer/index.tsx
@@ -1,15 +1,20 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import styled from '@emotion/styled';
+import { useInView } from 'react-intersection-observer';
 
+import ChatComponent from '~/atoms/Chat';
 import GlassContainer from '~/atoms/GlassContainer';
 import useSocketOn from '~/hooks/useSocketOn';
-import ChatList from '~/molecules/ChatList';
 import { Chat } from '~/types/Chat';
 import { SocketEvents } from '~/types/SocketEvents';
 
 interface Props {
   type: 'leftTitle' | 'rightTitle' | 'leftCharacter' | 'rightChat';
+}
+interface OptimizeChat {
+  allChat: Chat[];
+  subChat: Chat[];
 }
 
 const Container = styled(GlassContainer)<Props>`
@@ -24,27 +29,67 @@ const ChatListContainer = styled(Container)`
   overflow-y: scroll;
 `;
 
+const CHAT_BLOCK_SIZE = 50;
+
 const GameRoomChatContainer = () => {
-  const [chatList, setChatList] = useState<Chat[]>([]);
+  const [chatList, setChatList] = useState<OptimizeChat>({ allChat: [], subChat: [] });
   const chatListContainer = useRef<HTMLDivElement>(null);
+  const [topRef, isTopInView] = useInView();
 
   const scorllToBottom = () => {
     if (!chatListContainer.current) return;
     chatListContainer.current.scrollTo({ top: chatListContainer.current.scrollHeight, behavior: 'smooth' });
   };
+  const updateChat =
+    ({ name, text, status, color }: Chat) =>
+    (prevState: OptimizeChat) => {
+      const newChat = { name, text, status, color };
+      const newState = { ...prevState };
+      if (prevState.subChat.length >= CHAT_BLOCK_SIZE) {
+        newState.subChat.shift();
+      }
+      newState.subChat.push(newChat);
+      newState.allChat.push(newChat);
+      return newState;
+    };
+  const addSubChat = (prevState: OptimizeChat) => {
+    const newState = { ...prevState };
+    const start = newState.allChat.length - newState.subChat.length - CHAT_BLOCK_SIZE;
+    const end = newState.allChat.length;
+    newState.subChat = newState.allChat.slice(start < 0 ? 0 : start, end);
+    return newState;
+  };
+
+  useEffect(() => {
+    if (!isTopInView) return;
+    if (chatList.subChat.length < CHAT_BLOCK_SIZE) return;
+    setChatList(addSubChat);
+    if (!chatListContainer.current || chatList.allChat.length === chatList.subChat.length) return;
+    const chatGap = chatList.allChat.length - chatList.subChat.length;
+    const numOfAddedChat = chatGap >= 50 ? 50 : chatGap;
+    const CHAT_LINE_HEIGHT = 16;
+    chatListContainer.current.scrollTo(0, numOfAddedChat * CHAT_LINE_HEIGHT);
+  }, [isTopInView]);
+
   useSocketOn(SocketEvents.RECEIVE_CHAT, ({ name, text, status, color }: Chat) => {
-    setChatList((v) => [...v, { name, text, status, color }]);
+    setChatList(updateChat({ name, text, status, color }));
     scorllToBottom();
   });
 
   useSocketOn(SocketEvents.RECEIVE_ANSWER, ({ name, text, status }: Chat) => {
-    setChatList((v) => [...v, { name, text, status }]);
+    setChatList(updateChat({ name, text, status }));
     scorllToBottom();
   });
 
   return (
     <ChatListContainer type={'rightChat'} ref={chatListContainer}>
-      <ChatList chatList={chatList} />
+      <ul>
+        {chatList.subChat.map((element: Chat, i: number) => (
+          <li ref={!i ? topRef : null} key={i}>
+            <ChatComponent {...element} />
+          </li>
+        ))}
+      </ul>
     </ChatListContainer>
   );
 };
