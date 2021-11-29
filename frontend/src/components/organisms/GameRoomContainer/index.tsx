fd69ba2b0bde5c2e7bd607176b52173365aec5e1
guiyomj <wastfg6972@naver.com>
2021-11-29 10:35:38 +0900

    fix: 힌트 라운드 넘어가도 초기화 안되는 오류 수정

--- frontend/src/components/organisms/GameRoomContainer/index.tsx
+++ frontend/src/components/organisms/GameRoomContainer/index.tsx
@@ -185,7 +185,7 @@ const GameRoomContainer = ({ gameRoom, endTime }: { gameRoom: GameRoom; endTime:
 
   useEffect(() => {
     setHint('');
-  }, [gameRoom?.status]);
+  }, [gameRoom?.status, gameRoom?.curRound]);
 
   return (
     <>
